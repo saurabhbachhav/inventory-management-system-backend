4872f0386c9273e764e2f1b08039cd06d2b75d40
saurabhbachhav <bachhavsaurabh7@gmail.com>
2024-08-14 00:42:34 +0530

    Add User & Video model

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    vedioFile: {
+      type: String, //cloudnary url;
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudnary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudnary url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
